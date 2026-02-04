d6c9386814f24fb26ccbfafe204a4f790d530d4b
Muhammad Bilal Zulfiqar <145447653+bilalzulfiqar-pk@users.noreply.github.com>
2026-02-04 21:50:00 +0500

    Update Roman to Integer.py

--- leetcode/Roman to Integer/Roman to Integer.py
+++ leetcode/Roman to Integer/Roman to Integer.py
@@ -23,3 +23,29 @@ class Solution(object):
 
         return total
 
+# Faster
+
+# class Solution(object):
+#     def romanToInt(self,s):
+#         roman_map = {
+#             'I': 1,
+#             'V': 5,
+#             'X': 10,
+#             'L': 50,
+#             'C': 100,
+#             'D': 500,
+#             'M': 1000
+#         }
+        
+#         result = 0
+#         prev_value = 0
+        
+#         for char in reversed(s):
+#             curr_value = roman_map[char]
+#             if curr_value < prev_value:
+#                 result -= curr_value
+#             else:
+#                 result += curr_value
+#             prev_value = curr_value
+        
+#         return result
