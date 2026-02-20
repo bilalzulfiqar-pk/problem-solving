754dbf47caafcb75a0f42b4da7fba7794843bae0
Muhammad Bilal Zulfiqar <145447653+bilalzulfiqar-pk@users.noreply.github.com>
2026-02-20 12:59:09 +0500

    Update Fibonacci Number.py

--- leetcode/Easy/Fibonacci Number/Fibonacci Number.py
+++ leetcode/Easy/Fibonacci Number/Fibonacci Number.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # Time: O(n) Space:O(1)
         if n <= 1:
             return n
         
@@ -16,3 +17,39 @@ class Solution(object):
             prev2 = current
         
         return prev2
+
+        # Recursion method
+        # Time: O(2^n) Space: O(n)
+        # if n<=1:
+        #     return n
+        # else:
+        #     return self.fib(n-1) + self.fib(n-2)
+
+# Top-Down (Memoization) Time: O(n) Space: O(n)
+# class Solution(object):
+#     def fib(self, n, ht=None):
+
+#         if ht is None:
+#             ht = {0: 0, 1: 1} 
+
+#         if n in ht:
+#             return ht[n]
+        
+#         ht[n] = self.fib(n - 1, ht) + self.fib(n - 2, ht)
+#         return ht[n]
+
+# Bottom-Up (Tabulation) Time: O(n) Space: O(n)
+# class Solution(object):
+#     def fib(self, n):
+#         if n <= 1:
+#             return n
+        
+#         # Create the table (DP array)
+#         dp = [0] * (n + 1)
+#         dp[1] = 1
+        
+#         # Fill the table iteratively
+#         for i in range(2, n + 1):
+#             dp[i] = dp[i - 1] + dp[i - 2]
+            
+#         return dp[n]
